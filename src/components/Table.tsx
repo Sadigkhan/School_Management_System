30e80e00ab197f9c5ae85649e2271a070e0c9008
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2025-02-24 02:10:42 +0400

    Begining of the reusable Table component

---
+++ src/components/Table.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Table = () => {
+  return (
+    <table className='w-full mt-4'>
+        <th>Info</th>
+        <th>Teacher ID</th>
+        <th>Info</th>
+    </table>
+  )
+}
+
+export default Table
