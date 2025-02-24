e375b594f79bf565ff65fdd9bcc642e3e19decf4
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2025-02-24 18:23:15 +0400

    Teachers and Students List UI Completed Successfully

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React from "react";
 
-const Table = () => {
+const Table = ({
+  columns,
+  renderRow,
+  data
+}: {
+  columns: { header: string; accessor: string; className?: string }[];
+  renderRow: (item: any) => React.ReactNode;
+  data:any[];
+}) => {
   return (
-    <table className='w-full mt-4'>
-        <th>Info</th>
-        <th>Teacher ID</th>
-        <th>Info</th>
+    <table className="w-full mt-4">
+      <thead>
+        <tr className="text-left text-gary-500 text-sm">
+          {columns.map((col) => (
+            <th key={col.accessor} className={col.className}>{col.header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+          {data.map((item)=>renderRow(item))}
+      </tbody>
     </table>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
