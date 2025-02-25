63ef9694a3c4d4a9edea34369d2728dcd68bbc6e
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2025-02-25 18:33:25 +0400

    Begining og the Teacher Detail page

---
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SingleTeacherPage = () => {
+  return (
+    <div className='flex-1 p-4 flex flex-col gap-4 xl:flex-row'>
+        {/* LEFT */}
+        <div className='w-full xl:w-2/3'>L</div>
+        {/* RIGHT */}
+        <div className='w-full xl:w-1/3'>R</div>
+    </div>
+  )
+}
+
+export default SingleTeacherPage
