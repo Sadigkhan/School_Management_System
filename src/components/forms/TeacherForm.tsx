8470f105e6fa3293ba2964b618c65b1fdf212d9b
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2025-02-27 02:18:15 +0400

    Delete form created successfully

---
+++ src/components/forms/TeacherForm.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const TeacherForm = ({type,data}:{type: "create" | "update"; data?: any}) => {
+  return (
+    <form action="">
+        <input type="text"/>
+    </form>
+  )
+}
+
+export default TeacherForm
