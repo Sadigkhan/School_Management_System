710af585436551ab18af52c4138a128e58d5eedc
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2025-02-27 17:20:27 +0400

    TeacherForm Created Successfully

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,5 +1,6 @@
 import Announcements from "@/components/Announcements";
 import BigCalendar from "@/components/BigCalendar";
+import FormModal from "@/components/FormModal";
 import Performance from "@/components/Performance";
 import Image from "next/image";
 import Link from "next/link";
@@ -24,7 +25,27 @@ const SingleTeacherPage = () => {
               />
             </div>
             <div className="w-2/3 flex flex-col justify-between gap-4">
+              <div className="flex items-center gap-4">
               <h1 className="text-xl font-semibold">John Doe</h1>
+              <FormModal table="teacher" type="update" data={
+                 {
+                  id:1,
+                  username: "John Doe",
+                  email: "john@gmail.com",
+                  password: "123456",
+                  firstName: "John",
+                  lastName: "Doe",
+                  phone: "+994 (51) 123 45 67",
+                  address: "123 Main St, Anytown, USA",
+                  bloodType: "A+",
+                  birthDay: "1990-01-01",
+                  sex: "male",
+                  img: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200",
+                  
+                 } 
+              }/>
+              </div>
+              
               <p className="text-sm text-gray-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
               <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
                 <div className=" w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
