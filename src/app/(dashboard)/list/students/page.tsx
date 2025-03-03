347f93ad84d38d7871b7784fe14c6e95964e38bf
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2025-03-03 14:02:04 +0400

    ParentsList page data fetching ready. Next Subjects

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -102,6 +102,9 @@ const StudentListPage = async ({
             break;
           case "search":
             query.name = {contains:value,mode:"insensitive"}
+            break;
+            default:
+              break;
           
         }
       }
