df875791b43ffff5e08b8d00b6c16ed1683246d0
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2025-03-04 17:05:52 +0400

    Clerk Custom SignIn Component Creation

---
+++ src/app/[[...sign-in]]/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import * as Clerk from "@clerk/elements/common";
+import * as SignIn from "@clerk/elements/sign-in";
+import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
+
+import React from "react";
+
+const LoginPage = () => {
+
+    const {isLoaded,isSignedIn,user} = useUser();
+
+    console.log(user)
+  return (
+    <div className="h-screen flex items-center justify-center bg-eduSkyLight">
+      <SignIn.Root>
+        <SignIn.Step
+          name="start"
+          className="bg-white p-12 rounded-md shadow-2xl flex flex-col gap-2"
+        >
+          <h1 className="text-xl font-bold fleex items-center gap-2">
+            <Image src="/logo.png" alt="logo-image" width={24} height={24}/>
+            Education Management System
+          </h1>
+          <h2 className="text-gray-400">Sign In to your account</h2>
+          <Clerk.GlobalError className="text-sm text-red-400" />
+
+          <Clerk.Field name="identifier" className="flex flex-col gap-2">
+            <Clerk.Label className="text-xs text-gray-500">Username</Clerk.Label>
+            <Clerk.Input type="text" required className="p-2 rounded-md ring-1 ring-gray-300"/>
+            <Clerk.FieldError className="text-xs text-red-400"/>
+          </Clerk.Field>
+
+          <Clerk.Field name="password" className="flex flex-col gap-2">
+            <Clerk.Label className="text-xs text-gray-500">Password</Clerk.Label>
+            <Clerk.Input type="password" required className="p-2 rounded-md ring-1 ring-gray-300"/>
+            <Clerk.FieldError className="text-xs text-red-400"/>
+          </Clerk.Field>
+          
+          <SignIn.Action submit className="bg-blue-500 text-white my-1 rounded-md text-sm p-[10px]">Sign In</SignIn.Action>
+        </SignIn.Step>
+      </SignIn.Root>
+    </div>
+  );
+};
+
+export default LoginPage;
