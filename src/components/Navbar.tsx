c5b63cbe80af3775b6f7cf835df5f5f5bdd7ebe6
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2025-03-05 02:25:12 +0400

    Fetching data according to user Role

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,11 @@
 import { UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server'
 import Image from 'next/image'
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = async () => {
+    const user = await currentUser();
+    const role = user?.publicMetadata.role as string
   return (
     <div className='flex items-center justify-between p-4'>
         {/* SEARCH-BAR */}
@@ -24,7 +27,7 @@ const Navbar = () => {
             </div>
             <div className='flex flex-col'>
                 <span className='text-xs leading-3 font-medium'>Sadigkhan Gaykhanov</span>
-                <span className='text-[10px] text-gray-500 text-right'>Admin</span>
+                <span className='text-[10px] text-gray-500 text-right'>{role}</span>
             </div>
             {/* <Image src="/avatar.png" alt='user-image' width={36} height={36} className='rounded-full'/> */}
             <UserButton/>
