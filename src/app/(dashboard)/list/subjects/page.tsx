4107efd9f67f526a3097c853710aad0e649955fe
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2025-03-05 18:23:42 +0400

    Role Base Route Protection and Role Base Data Fetching Completed.

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -2,59 +2,62 @@ import FormModal from '@/components/FormModal'
 import Pagination from '@/components/Pagination'
 import Table from '@/components/Table'
 import TableSearch from '@/components/TableSearch'
-import {role,} from '@/lib/data'
 import { prisma } from '@/lib/prisma'
 import { ITEM_PER_PAGE } from '@/lib/settings'
+import { getRole } from '@/lib/utils'
 import { Prisma, Subject, Teacher } from '@prisma/client'
 import Image from 'next/image'
-import Link from 'next/link'
 import React from 'react'
 
 type SubjectList = Subject & {teachers:Teacher[]}
-
-const columns = [
-    {
-        header:"Subject Name",
-        accessor:"name"
-    },
-    {
-        header:"Teachers",
-        accessor:"teachers",
-        className:"hidden md:table-cell"
-    },  
-    {
-        header:"Actions",
-        accessor:"actions",
-    }
-]
-const renderRow = (item:SubjectList) =>(
-    <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-eduPurpleLight'>
-    <td className='flex items-center gap-4 p-4'>
-    {item.name}
-    </td>
-    <td className='hidden md:table-cell'>{item.teachers.map(teacher=>teacher.name).join(",")}</td>
-    <td>
-        <div className='flex items-center gap-2'>
-
-            {role==="admin"
-                &&
-                (
-                    <>
-                    <FormModal table="subject" type="update" data={item} />
-                    <FormModal table="subject" type="delete" id={item.id} />
-                    </>
-                )
-            }
-        </div>
-    </td>
-</tr>
-)
 
 const SubjectListPage = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) => {
+  
+  const role = await getRole()
+
+  const columns = [
+      {
+          header:"Subject Name",
+          accessor:"name"
+      },
+      {
+          header:"Teachers",
+          accessor:"teachers",
+          className:"hidden md:table-cell"
+      },  
+      ...(role === "admin" ? [
+        {
+          header:"Actions",
+          accessor:"actions",
+      }
+      ] : [])
+  ]
+  const renderRow = (item:SubjectList) =>(
+      <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-eduPurpleLight'>
+      <td className='flex items-center gap-4 p-4'>
+      {item.name}
+      </td>
+      <td className='hidden md:table-cell'>{item.teachers.map(teacher=>teacher.name).join(",")}</td>
+      <td>
+          <div className='flex items-center gap-2'>
+  
+              {role==="admin"
+                  &&
+                  (
+                      <>
+                      <FormModal table="subject" type="update" data={item} />
+                      <FormModal table="subject" type="delete" id={item.id} />
+                      </>
+                  )
+              }
+          </div>
+      </td>
+  </tr>
+  )
   const { page, ...queryParams } = searchParams;
 
   const p = page ? parseInt(page) : 1;
