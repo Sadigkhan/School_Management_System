4f2f70d6dc53f1a3dc2e1ef897a78f5a69a8f97d
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2025-03-06 14:58:58 +0400

    Big Calendar dynamic lesson views completed successfully.

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,16 +1,31 @@
 import Announcements from '@/components/Announcements'
-import BigCalendar from '@/components/BigCalendar'
+import BigCalendarContainer from '@/components/BigCalendarContainer'
 import EventCalendar from '@/components/EventCalendar'
+import { prisma } from '@/lib/prisma'
+import { getUserId } from '@/lib/utils'
 import React from 'react'
 
-const StudentPage = () => {
+const StudentPage = async() => {
+
+  const currentUserId = await getUserId();
+
+  const classItem = await prisma.class.findFirst({
+    where:{
+      students:{
+        some:{
+          id:currentUserId!
+        }
+      }
+    }
+  }) 
+
   return (
     <div className='p-4 flex gap-4 flex-col xl:flex-row'>
       {/* LEFT */}
       <div className='w-full xl:w-2/3'>
         <div className='h-full bg-white p-4 rounded-md'>
           <h1 className='text-xl font-semibold'>Schedule (P204)</h1>
-          <BigCalendar/>
+          <BigCalendarContainer type='classId' id={classItem?.id!} />
         </div>
       </div>
       {/* RIGHT */}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,74 @@
 import { auth } from "@clerk/nextjs/server";
 
+// for getting user's role and user's id
 export const getRole = async () => {
   const { sessionClaims } = await auth();
   return (sessionClaims?.metadata as { role: string })?.role;
 };
-
 
 export const getUserId = async () =>{
   const {userId} =await auth();
   
   return userId;
 }
+
+//For calendar
+const currentWorkWeek =()=>{
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+  const startOfWeek = new Date(today);
+
+  if(dayOfWeek === 0){
+    startOfWeek.setDate(today.getDate() +1);
+  }
+  else if(dayOfWeek ===6){
+    startOfWeek.setDate(today.getDate() +2);
+  }else{
+    startOfWeek.setDate(today.getDate()- (dayOfWeek-1));
+  }
+
+
+  startOfWeek.setHours(0,0,0,0);
+
+
+  return startOfWeek;
+};
+
+export const adjustScheduleToCurrentWorkWeek = (lessons:{title:string;start:Date;end:Date}[]):{title:string; start:Date;end:Date}[] =>{
+  const startOfWeek = currentWorkWeek();
+
+  return lessons.map((lesson)=>{
+    const lessonDayOfWeek = lesson.start.getDay();
+
+    const daysFromMonday = lessonDayOfWeek===0 ? 6 : lessonDayOfWeek -1;
+
+    const adjustedStartDate = new Date(startOfWeek);
+
+    adjustedStartDate.setDate(startOfWeek.getDate() +  daysFromMonday);
+    adjustedStartDate.setHours(
+      lesson.start.getHours(),
+      lesson.start.getMinutes(),
+      lesson.start.getSeconds(),
+    )
+
+
+    const adjustedEndDate = new Date(adjustedStartDate);
+    adjustedEndDate.setHours(
+      lesson.end.getHours(),
+      lesson.end.getMinutes(),
+      lesson.end.getSeconds(),
+    )
+
+    return {
+      title:lesson.title,
+      start:adjustedStartDate,
+      end:adjustedEndDate,
+    }
+
+
+  })
+};
+
+
+
 
